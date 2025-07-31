29296fc3accd59e2f1609f1c0a3e9d993146a582
Yaroslav Grachev <tuulwq@gmail.com>
2025-08-01 02:47:48 +0300

    feat: user store

--- src/stores/users/userStore.ts
+++ src/stores/users/userStore.ts
@@ -4,8 +4,8 @@ import type { User } from './types';
 import { delayed } from '../utils/delayed';
 import { USER_MOCK } from '@/mocks/users';
 
-function getNextLevelXp(currentXp: number, earnedXp: number, nextLevelXp: number) {
-  const overflownXp = currentXp + earnedXp - nextLevelXp;
+function getNextLevelXp(totalXp: number, nextLevelXp: number) {
+  const overflownXp = totalXp - nextLevelXp;
 
   if (overflownXp >= 0) {
     return Math.ceil(overflownXp / 100) * 100 + nextLevelXp;
@@ -15,7 +15,7 @@ function getNextLevelXp(currentXp: number, earnedXp: number, nextLevelXp: number
 }
 
 export const useUserStore = defineStore('user', () => {
-  const user = ref<User | null>(null);
+  const user = ref<User>();
 
   function requestUser() {
     delayed(() => USER_MOCK, 2000).then((data) => {
@@ -26,10 +26,12 @@ export const useUserStore = defineStore('user', () => {
   function earnXp(xp: number) {
     if (!user.value) return;
 
+    const totalXp = user.value.xp + xp;
+
     user.value = {
       ...user.value,
-      xp: user.value.xp + xp,
-      nextLevelXp: getNextLevelXp(user.value.xp, xp, user.value.nextLevelXp),
+      xp: totalXp,
+      nextLevelXp: getNextLevelXp(totalXp, user.value.nextLevelXp),
     };
   }
 
